599a9f9e75b298dbbafe22f100410aecb9988702
David Gage <whytheplatypus@gmail.com>
2014-07-08 13:39:43 -0400

    more work with polymer-elements

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,8 @@
 angular.module('chessApp')
   .controller('MainCtrl', function ($scope) {
     $scope.uniq =  Math.random().toString(36).substr(2, 9);
+    $scope.paper_modal = function(id, event){
+      event.preventDefault();
+      document.getElementById(id).toggle();
+    }
   });
