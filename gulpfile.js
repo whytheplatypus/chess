ba9d553bcbbf4574f1b97d53e11930dca9771a20
David Gage <whytheplatypus@gmail.com>
2014-07-11 15:24:07 -0400

    gulp is behaving itself

--- gulpfile.js
+++ gulpfile.js
@@ -28,9 +28,8 @@ gulp.task('connect', function () {
     var connect = require('connect');
     var app = connect()
         .use(require('connect-livereload')({ port: 35729 }))
-        .use(connect.static('app'))
-        .use(connect.static('.tmp'))
-        .use(connect.directory('app'));
+        .use(connect.static('./'))
+        .use(connect.directory('./'));
 
     require('http').createServer(app)
         .listen(9000)
